9d04fd80fa1b046a351d56f61ba66c9530b274f3
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-06 00:47:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:keys => 4
+				:key_one => 1,
+				:key_two => 1,
+				:key_three => 1,
+				:key_four => 1
 			}
 				}
 	}
