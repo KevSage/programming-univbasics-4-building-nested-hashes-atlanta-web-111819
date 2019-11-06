57b581a80d2dd5ef5a146f8d80bb04b7891f1d8f
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-06 01:00:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,16 +17,16 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:key_one => 1,
-				:key_two => 1,
-				:key_three => 1,
-				:key_four => 1
+				:one_piece_owned=> 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 75,
+				:four_pieces_owned => 100
 			},
 			:names => {
-        :one_piece_owned => 25,
-				:two_piece_owned => 1,
-				:three_piece_owned => 1,
-				:four_piece_owned => 1,
+        :one => 1,
+				:two => 1,
+				:three => 1,
+				:four => 1,
 
 			}
 				}
